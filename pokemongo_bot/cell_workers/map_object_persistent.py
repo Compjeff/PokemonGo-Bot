75f13f7b038e90a5a56b732416accb47fe2f081c
Compjeff <jeffye@jeffs-MacBook-Pro.local>
2016-08-11 23:45:04 -0400

    loop throught cells info

--- pokemongo_bot/cell_workers/map_object_persistent.py
+++ pokemongo_bot/cell_workers/map_object_persistent.py
@@ -5,7 +5,40 @@ class MapObjectPersistent(BaseTask):
     SUPPORTED_TASK_API_VERSION = 1
 
     def initialize(self):
+        self.last_map_object_time = 0;
         print self.config
 
     def work(self):
-        print "dsdsds ddss"
+        if not self.bot.last_map_object:
+            return
+
+        if self.bot.last_time_map_object == self.last_map_object_time:
+            return
+
+        if 'responses' not in self.bot.last_map_object:
+            return
+
+        if 'GET_MAP_OBJECTS' not in self.bot.last_map_object['responses']:
+            return
+
+        dict = self.bot.last_map_object['responses']['GET_MAP_OBJECTS']
+        if 'map_cells' not in dict:
+            return;
+
+        for cell in dict['map_cells']:
+            s2_cell_id = cell['s2_cell_id']
+            if 'forts' in cell:
+                for fort in cell['forts']:
+                    if 'owned_by_team'in fort:
+                        pass
+                    if 'type' in fort:
+                        if fort['type'] == 1:
+                            pass
+            if 'catchable_pokemons' in cell:
+                pass
+            if 'wild_pokemons' in cell:
+                pass
+            if 'nearby_pokemons' in cell:
+                pass
+            if 'spawn_points' in cell:
+                pass
